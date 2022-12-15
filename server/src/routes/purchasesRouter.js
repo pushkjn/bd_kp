a26849d12bff3b71565473477f73b21ea972b53f
vlask <vlad.dondokov@gmail.com>
2022-12-15 23:49:38 +0300

    little fix

--- server/src/routes/purchasesRouter.js
+++ server/src/routes/purchasesRouter.js
@@ -6,5 +6,5 @@ const purchasesController = require('../controlers/purchasesController')
 router.get('/all', purchasesController.getAllPurchases)
 router.get('/:id', purchasesController.getPurchaseById)
 router.post('/add', purchasesController.addPurchase)
-router.delete('/delete/:id', purchasesController.deletePurchasesge)
+router.delete('/delete/:id', purchasesController.deletePurchases)
 module.exports = router
